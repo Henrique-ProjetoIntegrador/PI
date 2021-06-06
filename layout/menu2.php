091e3212366b88c3ae46f98828cd39e4f3386fee
Bruna <bruna.pe13@hotmail.com>
2021-06-06 15:49:09 -0300

    iniciando versao2 indexmenu

---
+++ layout/menu2.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+
+    <link rel="stylesheet" href="styles/menu.css"/>
+    <link rel="stylesheet" href="styles/index.css"/>
+    <title>HOME</title>
+</head>
+<body>
+<header>
+    <nav id="menu">
+        <ul>
+            <li><a href="index.html">HOME</a></li>
+                <li><a href="telas/clientes.html">CLIENTES</a></li>
+                <li><a href="veiculos.html">VEÍCULOS</a></li>
+                <li><a href="orcamentos.html">ORÇAMENTOS</a></li>
+                <li><a href="pecas.html">PEÇAS</a></li>
+        </ul>
+    </nav>
+</header>
