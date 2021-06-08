e2b368d1f5f77e7c58fec817bd0100fd1482456e
henriquesd11 <henriquesd11@hotmail.com>
2021-06-08 19:41:33 -0300

    ajuste na padronização das telas, cirando diretório para cada tela, mantendo assim uma organizaçao melhor

--- telas/layout/menu.php
+++ telas/layout/menu.php
@@ -5,19 +5,19 @@
                 <ul>
                     <div class="row text-center">
                     <div class="col-3">
-                        <li><a href="index.php"> <h1>HOME</h1></a></li>
+                        <li><a href="../principal/index.php"> <h1>HOME</h1></a></li>
                     </div>
                     <div class="col-2">
-                        <li><a href="telas/clientes.php"> <h1>clientes</h1></a></li>
+                        <li><a href="../cliente/clientes.php"> <h1>clientes</h1></a></li>
                     </div>
                     <div class= "col-3">
-                        <li><a href="telas/veiculo.php"> <h1>veículos</h1></a></li>
+                        <li><a href="../veiculo/veiculo.php"> <h1>veículos</h1></a></li>
                     </div>
                     <div class= "col-2">
-                        <li><a href=""><h1>orçamentos</h1></a></li>
+                        <li><a href="../orcamento/"><h1>orçamentos</h1></a></li>
                     </div>
                     <div class= "col-2">
-                        <li><a href=""><h1>peças</h1></a></li>
+                        <li><a href="../pecas/"><h1>peças</h1></a></li>
                     </div>
                     
                     <div class ="col-2 offset-10">
