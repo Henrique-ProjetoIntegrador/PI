71e60866b6be3b4af61a0cb1d5251a464f4e9b10
Bruna <bruna.pe13@hotmail.com>
2021-06-09 18:03:04 -0300

    menu

--- telas/principal/index.php
+++ telas/principal/index.php
@@ -22,38 +22,34 @@
     ?>
 </header>
 <div class="row text-center">
-    <div class="imagem-usuarios col-lg-4 offset-1">
+    <div class="imagem-usuarios col-lg-3">
         <figure class="figure">
-            <a href="usuario.html"><img src="../../icones/Usuario.png" class="figure-img img-fluid rounded"
-                                        alt="Ícone usuários" width=250 height=250></a>
-            <figcaption class="figure-caption">GERENCIAMENTO DE USUÁRIOS</figcaption>
+            <a href="usuario.html"><img src="../../icones/Usuario.png" class="figure-img img-fluid rounded"alt="Ícone usuários"></a>
+            <figcaption class="legenda-usuarios figure-caption">GERENCIAMENTO DE USUÁRIOS</figcaption>
         </figure>
     </div>
-    <div class="imagem-peças col-lg-3">
+    <div class="imagem-pecas col-lg-3">
         <figure class="figure">
-            <a href="pecas.html"><img src="../../icones/pecas.png" class="figure-img img-fluid rounded" alt="Ícone peças."
-                                      width=250 height=250></a>
-            <figcaption class="figure-caption">GERENCIAMENTO DE PEÇAS</figcaption>
+            <a href="pecas.html"><img src="../../icones/pecas.png" class="figure-img img-fluid rounded" alt="Ícone peças."></a>
+            <figcaption class="legenda-pecas figure-caption">GERENCIAMENTO DE PEÇAS</figcaption>
         </figure>
     </div>
-    <div class="imagem-orcamentos col-lg-3">
+    <div class="imagem-orcamentos col-lg-4 offset-1">
         <figure class="figure">
-            <a href="orcamentos.html"><img src="../../icones/orcamentos.png" class="figure-img img-fluid rounded"
-                                           alt="Ícone Orçamentos." width=250 height=250></a>
-            <figcaption class="figure-caption">GERENCIAMENTO DE ORÇAMENTOS</figcaption>
+            <a href="orcamentos.html"><img src="../../icones/orcamentos.png" class="figure-img img-fluid rounded" alt="Ícone Orçamentos."></a>
+            <figcaption class=" legenda-orcamentos figure-caption">GERENCIAMENTO DE ORÇAMENTOS</figcaption>
         </figure>
     </div>
     <div class="imagem-clientes col-lg-4 offset-2">
         <figure class="figure">
-            <a href="../cliente/clientes.php"><img src="../../icones/Clientes.webp" class="figure-img img-fluid rounded"
-                                                   alt="Ícone Clientes" width=250 height=250></a>
-            <figcaption class="figure-caption">GERENCIAMENTO DE CLIENTES</figcaption>
+            <a href="../cliente/clientes.php"><img src="../../icones/Clientes.webp" class="figure-img img-fluid rounded"alt="Ícone Clientes"></a>
+            <figcaption class="legenda-clientes figure-caption">GERENCIAMENTO DE CLIENTES</figcaption>
         </figure>
     </div>
     <div class="imagem-veiculos col-lg-4 ">
         <figure class="figure">
-            <a href="../veiculo/veiculo.php"><img src="../../icones/veiculo.png" class="figure-img img-fluid rounded" alt="Ícone veículo" width=250 height=250></a>
-            <figcaption class="figure-caption">GERENCIAMENTO DE VEICULOS</figcaption>
+            <a href="../veiculo/veiculo.php"><img src="../../icones/veiculo.png" class="figure-img img-fluid rounded" alt="Ícone veículo"></a>
+            <figcaption class="legenda-veiculos figure-caption">GERENCIAMENTO DE VEICULOS</figcaption>
         </figure>
     </div>
 </div>
