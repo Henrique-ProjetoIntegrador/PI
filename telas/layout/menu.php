5f2cff4b3d3f4025d75f01ec42e577b2a43d813d
Bruna <bruna.pe13@hotmail.com>
2021-06-10 00:23:03 -0300

    .

--- telas/layout/menu.php
+++ telas/layout/menu.php
@@ -4,23 +4,23 @@
             <nav class="lista-menu">
                 <ul>
                     <div class="row text-center">
-                    <div class="col-3">
+                    <div class="col-sm-3">
                         <li><a href="../principal/index.php"> <h1>HOME</h1></a></li>
                     </div>
-                    <div class="col-2">
+                    <div class="col-sm-2">
                         <li><a href="../cliente/clientes.php"> <h1>clientes</h1></a></li>
                     </div>
-                    <div class= "col-3">
+                    <div class= "col-sm-3">
                         <li><a href="../veiculo/veiculo.php"> <h1>veículos</h1></a></li>
                     </div>
-                    <div class= "col-2">
+                    <div class= "col-sm-2">
                         <li><a href="../orcamento/"><h1>orçamentos</h1></a></li>
                     </div>
-                    <div class= "col-2">
+                    <div class= "col-sm-2">
                         <li><a href="../pecas/"><h1>peças</h1></a></li>
                     </div>
                     
-                    <div class ="col-2 offset-10">
+                    <div class ="col-sm-3 offset-9">
                         <a href="" class="sair"><u>Sair</u></a>
                     </div>
                 </ul>
--- telas/principal/index.php
+++ telas/principal/index.php
@@ -21,8 +21,62 @@
 
     ?>
 </header>
+
 <div class="row text-center">
-    <div class="imagem-usuarios col-lg-3">
+
+    <div class="imagem-usuarios col-sm-3">
+        <figure>
+            <a href="usuario.html"><img src="../../icones/Usuario.png" alt="Ícone usuários" width="63%"></a>
+            <figcaption class="leg-usuarios">GERENCIAMENTO DE USUÁRIOS</figcaption>
+    </figure>
+    </div>
+
+    <div class="imagem-clientes col-sm-2">
+        <figure>
+            <a href="../cliente/clientes.php"><img src="../../icones/Clientes.png" alt="Ícone Clientes."width="97%"></a>
+            <figcaption class="leg-clientes">GERENCIAMENTO DE CLIENTES</figcaption>
+        </figure>
+    </div>
+
+    <div class="imagem-veiculos col-sm-2">
+        <figure class="figure">
+            <a href="../veiculo/veiculo.php"><img src="../../icones/veiculo.png" alt="Ícone veículo"width="100%"></a>
+            <figcaption class="leg-veiculos">GERENCIAMENTO DE VEICULOS</figcaption>
+        </figure>
+    </div>
+
+<div class="imagem-pecas col-sm-2">
+    <figure>
+        <a href="pecas.html"><img src="../../icones/pecas.png" alt="Ícone peças"width="95%"></a>
+        <figcaption class="leg-pecas">GERENCIAMENTO DE PEÇAS</figcaption>
+    </figure>
+</div>
+<div class="imagem-orcamentos col-sm-3">
+        <figure class="figure">
+            <a href="orcamentos.html"><img src="../../icones/orcamentos.png" alt="Ícone Orçamentos." width="95%"></a>
+            <figcaption class="leg-orcamentos">GERENCIAMENTO DE ORÇAMENTOS</figcaption>
+        </figure>
+
+
+</div>
+
+<!-- <div class="imagem-clientes col-sm-6">
+        <figure class="figure">
+            <a href="../cliente/clientes.php"><img src="../../icones/Clientes.png" class="figure-img img-fluid rounded"alt="Ícone Clientes" ></a>
+            <figcaption class="legenda-clientes figure-caption">GERENCIAMENTO DE CLIENTES</figcaption>
+        </figure>
+    </div>
+
+    <div class="imagem-veiculos col-sm-6">
+        <figure class="figure">
+            <a href="../veiculo/veiculo.php"><img src="../../icones/veiculo.png" class="figure-img img-fluid rounded" alt="Ícone veículo"></a>
+            <figcaption class="legenda-veiculos figure-caption">GERENCIAMENTO DE VEICULOS</figcaption>
+        </figure>
+    </div>
+</div> -->
+
+<!-- <div class="row text-center">
+    <div class="imagem-usuarios col-sm-3">
         <figure class="figure">
             <a href="usuario.html"><img src="../../icones/Usuario.png" class="figure-img img-fluid rounded"alt="Ícone usuários"></a>
             <figcaption class="legenda-usuarios figure-caption">GERENCIAMENTO DE USUÁRIOS</figcaption>
@@ -52,6 +106,6 @@
             <figcaption class="legenda-veiculos figure-caption">GERENCIAMENTO DE VEICULOS</figcaption>
         </figure>
     </div>
-</div>
+</div> -->
 </body>
 </html>
