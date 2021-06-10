df04a616e3fbe66980b1eb07325c12f5797a154a
henriquesd11 <henriquesd11@hotmail.com>
2021-06-09 21:02:10 -0300

    Encaminhando todas as telas de veiculos ja prontas

--- telas/layout/menu.php
+++ telas/layout/menu.php
@@ -11,7 +11,7 @@
                         <li><a href="../cliente/clientes.php"> <h1>clientes</h1></a></li>
                     </div>
                     <div class= "col-3">
-                        <li><a href="../veiculo/veiculo.php"> <h1>veículos</h1></a></li>
+                        <li><a href="../veiculo/index.php"> <h1>veículos</h1></a></li>
                     </div>
                     <div class= "col-2">
                         <li><a href="../orcamento/"><h1>orçamentos</h1></a></li>
