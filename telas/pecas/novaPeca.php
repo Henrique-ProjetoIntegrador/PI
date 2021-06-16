e05461161cc429c65753cff8e17e3ced142763ee
Diegomurilo19 <diegomurilo2019@gmail.com>
2021-06-15 22:40:39 -0300

    finalizada a tela principal de peças e peças novas

--- telas/pecas/novaPeca.php
+++ telas/pecas/novaPeca.php
@@ -34,38 +34,38 @@
         <table class="table table-hover table-striped text-center col-sm-8">
                         <thead class="thead-dark">
                         <tr>
-                            <th style="width: 600px">CATEGORIA</th>
+                            <th style="width: 600px">
+                            <div class="campos-input">
+                            <div class="row">
+                                <label for="nome" class="col-sm-3">Nome da Peça</label>
+                                <input class="form form-control form-control-sm col-sm-5" id="name" type="name" required/>
+                            </div>
+                            
+                            <div class="row">
+                                <label for="nome" class="col-sm-3">Categoria da Peça</label>
+                            <select name="select">
+                            <option value="valor1" selected>Selecione</option>
+                            <option value="valor2" >Ar Condicionado</option>
+                            <option value="valor3">Borracharia</option>
+                            <option value="valor3">Direção</option>
+                            <option value="valor3">Elétrica</option>
+                            <option value="valor3">Freio</option>
+                            <option value="valor3">Injeção</option>
+                            <option value="valor3">Motor</option>
+                            
+                            </select>
+                            </div>
+                            </div>
+                            </th>
                             
                         </tr>
                         </thead>
-                        <tbody>
-                        <tr>
-                            <th scope="row">Ar Condicionado</th>
-                            
-                        </tr>
-                        <tr>
-                            <th scope="row">Borracharia</th>
-                            
-                        </tr>
-                        <tr>
-                            <th scope="row">Direção</th>
-                            
-                        </tr>
-                        <tr>
-                            <th scope="row">Elétrica</th>
-                           
-                        </tr>
-                        <tr>
-                            <th scope="row">Freio</th>
-                            
-                        </tr>
-
-                        </tbody>
+                        
                     </table>
                     <div class="option col-3 offset-1">
                         <div class="row">
                         <div class="col-12">
-                                <a href="novaPeca.php" class="btn btn-danger btn-lg btn-block">Novo</a>
+                        <button id="salvar" name="salvar" type="Submit" class="btn btn-danger btn-lg btn-block">Salvar</button>
                                 <br>
                             </div>
                             <div class="col-12">
