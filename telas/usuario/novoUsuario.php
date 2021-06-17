942e3c488925d32839501b9a873e0957a8719172
Bruna <bruna.pe13@hotmail.com>
2021-06-16 21:55:20 -0300

    alteraçoes tela novoUsuario

--- telas/usuario/novoUsuario.php
+++ telas/usuario/novoUsuario.php
@@ -49,20 +49,10 @@
                 </div>
                 <?php
 
-                // o que eu fiz, ao invez de criar a classe do alerta varias vezes la na session
-                //isset, verifica se o que esta dentro do isset(aqui) existe ou nao, se sim retorna True,
-                // se nao retorna False
-                // e se True ele vai executar o echo $_SESSION['erroCampos'];
-                // se nao, ele nao vai executa nada 
-                // agr para nao fica aparecendo a mensagfem sempre, vc precisa elminar essa sessão
-                // usando o unset
-
-                // no caso aqui, e com isso eu apenas recebo a mensagem do erro que da, ja com a class do alerta pronto
-                // blz amor, tem alguma duvisa?, preciso volta a trabalhar ja voltooooooooooooooooooooooooooooooooooooooooooo
                     if(isset($_SESSION['erroCampos'])){
                         // criando a classe alerta
                         echo "<div class='alert alert-danger'>";
-                            //mensagem que vai estar no alerta sacou brow?
+                            
                             echo $_SESSION['erroCampos'];
                         echo "</div>";
                         // fechando o div da class alerta
@@ -70,7 +60,7 @@
                     }
                     // apenas isso ja vai fazer com que mostra a mensagem caso os campos estejam vazios
                     // mas antes é precisa verificar se essa sessao existe, no caso criar uma condição
-                    // para verificar isso
+                   
                 ?>          
         </div>
         <div class="options-buttons col-sm-2">
@@ -85,29 +75,6 @@
             </div>
         </div>      
     </div> 
-        </form> 
-                    <!-- <div class="modal" id= "salvar" tabindex="-1" role="dialog">
-                            <div class= "modal-dialog" role="document">
-                                <div class= "modal-content">
-                                    <div class="modal-header">
-                                        <h5 class= "modal-title">Salvar</h5>
-                                        <button type="button" class="close" data-dismiss="modal">
-                                            <span aria-hidden="true">&times;</span>
-                                        </button>
-                                    </div>
-                                    <div class="modal-body">
-                                        <p>Salvo com sucesso!</p>
-                                    </div>
-                                    <div class="modal-footer">
-                                        <button type="button" class="btn btn-warning" data-dismiss="modal">Fechar</button>                
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                 
-                     -->
-                       
-        
-        
+        </form>    
 </body>
 </html>
